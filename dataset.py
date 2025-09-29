bbaa7cd72f0bd0fe7bb6b15a1efbf6c4e8f26d06
tjacek <jacekt89@gmail.com>
2025-09-29 23:21:47 +0200

    first commit

---
+++ dataset.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+
+class Dataset(object):
+    def __init__(self,X,y=None):
+        self.X=X
+        self.y=y
+
+    def __len__(self):
+        return len(self.y)
+
+    def dim(self):
+        return self.X.shape[1]
+        
+    def n_cats(self):
+        return int(max(self.y))+1
+
+    def fit_clf(self,train,clf):
+        X_train,y_train=self.X[train],self.y[train]
+        history=clf.fit(X_train,y_train)
+        return clf,history
+
+def read_csv(in_path:str):
+    if(type(in_path)==tuple):
+        X,y=in_path
+        return Dataset(X,y)
+    if(type(in_path)!=str):
+        return in_path
+    df=pd.read_csv(in_path,header=None)
+    raw=df.to_numpy()
+    X,y=raw[:,:-1],raw[:,-1]
+    X= preprocessing.RobustScaler().fit_transform(X)
+    return Dataset(X,y)
