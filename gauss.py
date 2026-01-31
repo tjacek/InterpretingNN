2788476033e44fa3e4766886b01d109bceff1983
tjacek <jacekt89@gmail.com>
2026-01-31 23:49:58 +0100

    Tree class

--- gauss.py
+++ gauss.py
@@ -1,5 +1,26 @@
 import numpy as np
 from numpy import random
+from dataclasses import dataclass
+
+@dataclass
+class Node(object):
+	feat:int
+	thres:float
+	left:int
+	right:int
+
+    def __call__(self,x_i):
+    	feat_i=x_i[self.feat]
+    	if(feat_i< self.thres):
+    		return self.left
+    	else:
+    		return self.right
+
+
+class Tree(object):
+	def __init__(self,nodes):
+		self.nodes=nodes
+
 
 def count_extr(x):
 	count=np.sum(x>2)
