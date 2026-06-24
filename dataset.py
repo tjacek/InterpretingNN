ce38e992f65b8948e8c6b3fb800a7c39a3e19a76
tjacek <jacekt89@gmail.com>
2026-06-24 23:53:56 +0200

    Split class

--- dataset.py
+++ dataset.py
@@ -69,6 +69,39 @@ class Dataset(object):
         thres_var= np.argmax(int_var)/cum_var.shape[0]
         return greatest,thres_var
 
+class Split(object):
+    def __init__(self,train_index,test_index):
+        self.train_index=train_index
+        self.test_index=test_index
+    
+    def fit_clf(self,data,clf):
+        return data.fit_clf(self.train_index,clf)
+
+    def pred(self,data,clf):
+        return data.pred(self.test_index,
+                         clf=clf)
+
+    def save(self,out_path):
+        return np.savez(out_path,self.train_index,self.test_index)
+
+    def __str__(self):
+        train_size=self.train_index.shape[0]
+        test_size=self.test_index.shape[0]
+        return f"train:{train_size},test:{test_size}"     
+    
+    @classmethod
+    def random(cls,n_samples,p=0.9):
+        if(type(n_samples)==Dataset):
+            n_samples=len(n_samples)
+        train,test=[],[]
+        for i in range(n_samples):
+            if(np.random.rand()<p):
+                train.append(i)
+            else:
+                test.append(i)
+        train,test=np.array(train),np.array(test)
+        return cls( train_index=train,
+                    test_index=test)
 class Result(object):
     def __init__(self,y_pred,y_true):
         self.y_pred=y_pred
