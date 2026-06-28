77f84ce0cef7737f67345ca01b4975834db5b9e5
tjacek <jacekt89@gmail.com>
2026-06-28 23:48:33 +0200

    SplitGroup

--- base.py
+++ base.py
@@ -1,33 +1,33 @@
 import numpy as np
 from sklearn.model_selection import RepeatedStratifiedKFold
 
-class SplitProtocol(object):
-    def __init__(self,n_splits,n_repeats):
-        self.n_splits=n_splits
-        self.n_repeats=n_repeats
+class SplitGroup(object):
+    def __init__(self,splits):#n_splits,n_repeats):
+        self.splits=splits
+ #       self.n_splits=n_splits
+ #       self.n_repeats=n_repeats
+    
+    def __len__(self):
+        return len(self.splits)
 
-    def get_split(self,data):
+    @classmethod
+    def get_split( cls,
+                   data,
+                   n_repeats=1,
+                   n_splits=10):
         rskf=RepeatedStratifiedKFold(n_repeats=self.n_repeats, 
                                      n_splits=self.n_splits, 
                                      random_state=0)
         splits=[]
         for train_index,test_index in rskf.split(data.X,data.y):
             splits.append(Split(train_index,test_index))
-        return splits
+        return cls(splits)
 
 class Split(object):
     def __init__(self,train_index,test_index):
         self.train_index=train_index
         self.test_index=test_index
-
-        
-    def eval(self,data,clf):
-        if(type(clf)==str):
-            clf=get_clf(clf)
-        return data.eval(train_index=self.train_index,
-                         test_index=self.test_index,
-                         clf=clf)
-       
+    
     def fit_clf(self,data,clf):
         return data.fit_clf(self.train_index,clf)
 
@@ -41,16 +41,18 @@ class Split(object):
     def __str__(self):
         train_size=self.train_index.shape[0]
         test_size=self.test_index.shape[0]
-        return f"train:{train_size},test:{test_size}"
-
-def random_split(n_samples,p=0.9):
-#    if(type(n_samples)==dataset.Dataset):
-#        n_samples=len(n_samples)
-    train_index,test_index=[],[]
-    for i in range(n_samples):
-        if(np.random.rand()<p):
-            train_index.append(i)
-        else:
-            test_index.append(i)
-    return Split(train_index=np.array(train_index),
-                 test_index=np.array(test_index))
+        return f"train:{train_size},test:{test_size}"     
+    
+    @classmethod
+    def random(cls,n_samples,p=0.9):
+        if(type(n_samples)==Dataset):
+            n_samples=len(n_samples)
+        train,test=[],[]
+        for i in range(n_samples):
+            if(np.random.rand()<p):
+                train.append(i)
+            else:
+                test.append(i)
+        train,test=np.array(train),np.array(test)
+        return cls( train_index=train,
+                    test_index=test)
