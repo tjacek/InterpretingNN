6e0e89c7c6c0e1ae1411e87413b973e3b1cc22cf
tjacek <jacekt89@gmail.com>
2026-07-05 13:51:38 +0200

    show different norms on one plot

--- nn_dist.py
+++ nn_dist.py
@@ -8,24 +8,37 @@ def neural_dist(in_path):
 	for id_i,path_i in paths.items():
 		mlp_i=deep.MLP.read(f"{path_i}/models/0.keras")
 		weights=mlp_i.get_weights()
-		norms=L1(weights).reshape(-1, 1)
-		kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(norms)
-		x=np.linspace(0,np.amax(norms), num=50).reshape(-1, 1)
-		y = np.exp(kde.score_samples(x))
+		x,y_dict=compute_density(weights,[L1(),L2()])
+		plot(x,y_dict,id_i)
 
-		plt.figure(figsize=(8, 5))
+def plot(x,y_dict,title):
+	plt.figure(figsize=(8, 5))
+	for id_i,y_i in y_dict.items():
 		plt.plot(
             x[:, 0],
-            y,
-            label=str(id_i)
+            y_i,
+            label=id_i
         )
-		plt.xlabel("L1 norm")
-		plt.ylabel("Density")
-		plt.title("Kernel density of neural weights")
-		plt.legend()
-		plt.grid(True)
-		plt.tight_layout()
-		plt.show()
+	plt.xlabel("Norm")
+	plt.ylabel("Density")
+	plt.title(title)
+	plt.legend()
+	plt.grid(True)
+	plt.tight_layout()
+	plt.show()
+
+def compute_density(weights,norm_fun):
+    y_dict,max_value={},0
+    for fun_i in norm_fun:
+        norms_i=fun_i(weights).reshape(-1, 1)
+        max_value=max(np.amax(norms_i),max_value)
+        kde = KernelDensity(kernel='gaussian', bandwidth=0.2)
+        kde.fit(norms_i)
+        y_dict[str(fun_i)]=kde
+    x=np.linspace(0,max_value, num=50).reshape(-1, 1)
+    y_dict ={ id_i:np.exp(kde_i.score_samples(x))
+                for id_i,kde_i in y_dict.items()}
+    return x,y_dict
 
 def mlp_paths(in_path):
 	return {id_i:[path_i 
@@ -33,8 +46,27 @@ def mlp_paths(in_path):
 	             if(id_i=="MLP")][0]
 	          for id_i,path_i in utils.iter_files(in_path)}
 
-def L1(weights):
-    weights=np.abs(weights)
-    return np.sum(weights,axis=0)
+class L1(object):
+    def __call__(self,weights):
+        weights=np.abs(weights)
+        return np.sum(weights,axis=0)
+
+    def __str__(self):
+    	return "L1"
+
+class L2(object):
+    def __call__(self,weights):
+        weights=weights**2
+        norm=np.sum(weights,axis=0)
+        return np.sqrt(norm)
+
+    def __str__(self):
+    	return "L2"
 
 neural_dist("uci/output")
+
+#weights=np.array([[4,3],
+#	              [3,4]])
+
+#print(L1()(weights))
+#print(L2()(weights))
