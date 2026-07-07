99ef23522e8417dd19f97fce81941401e67cf14d
tjacek <jacekt89@gmail.com>
2026-07-08 00:50:42 +0200

    leve one out

--- reg.py
+++ reg.py
@@ -24,15 +24,42 @@ def to_array(df):
     X=df.to_numpy()
     return X,y
 
+def leve_one_out(df):
+    for i in range(len(df)):
+        train = df.drop(index=i)
+
+        X_train = train.drop(columns=["target", "data"]).to_numpy()
+        y_train = train["target"].to_numpy()
+        train=dataset.Dataset(X_train,y_train)
+
+        X_test = df.iloc[[i]].drop(columns=["target", "data"]).to_numpy()
+        y_test = df.iloc[i]["target"]
+        test=dataset.Dataset(X_test,y_test)
+
+        data_i = df.iloc[i]["data"]
+
+        yield train,test, data_i
+
+def gpr(df):
+    names,y_true,y_pred,error=[],[],[],[]
+    for train_i,test_i,data_i  in leve_one_out(df):
+        kernel =  RBF(length_scale=1.0, 
+                      length_scale_bounds=(1e-2, 1e2))
+        gauss_process = GaussianProcessRegressor(kernel=kernel, 
+                                                    n_restarts_optimizer=9)
+        gauss_process.fit(train_i.X, train_i.y)
+        names.append(data_i)
+    
+        mean_i, std_i = gauss_process.predict(test_i.X, 
+                                              return_std=True)
+        y_pred.append(mean_i[0])
+        error.append(std_i[0])
+    return names,y_true,y_pred,error
+
 def gauss_reg(data_path,result_path):
     df=get_input_data(data_path,result_path)
-    X,y=to_array(df)
-    kernel =  RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e2))
-    gaussian_process = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
-    gaussian_process.fit(X, y)
-    mean_pred, std_pred = gaussian_process.predict(X, return_std=True)
-    print(mean_pred)
-    print(std_pred)
+    names,y_true,y_pred,error=gpr(df)
+
 #def reg_exp(data_path,result_path):
 #    df=get_input_data(data_path,result_path)
 #    X,y=to_array(df)
@@ -53,7 +80,7 @@ def gauss_reg(data_path,result_path):
 #                           ["data","true","pred","res"] )
 #    print(df_reg)
 
-gauss_reg([#"AutoML/data",
+gauss_reg(["AutoML/data",
            "uci/data"],
-        [#"AutoML/output",
-         "uci/output"])
+          ["AutoML/output",
+           "uci/output"])
