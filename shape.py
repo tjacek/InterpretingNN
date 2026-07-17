9f201f215948981cff1696a4cd5a48b5b2513230
tjacek <jacekt89@gmail.com>
2026-07-17 23:50:48 +0200

    split shapley values

--- shape.py
+++ shape.py
@@ -2,10 +2,38 @@ import numpy as np
 import shap
 from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from sklearn.metrics import accuracy_score
 import argparse
 import xgboost
 import base,dataset,plot,utils
+
+def rf_shapley( in_path,
+                split_path,
+                out_path):
+    split_dict=base.get_split_dict(split_path)
+    @utils.dir_fun
+    def helper(in_path,out_path=None):
+        data_id=in_path.split("/")[-1]
+        print(data_id)
+        data=dataset.read_csv(in_path)
+        splits=split_dict[data_id]
+        all_shape=[]
+        for split_i in tqdm(splits.splits):
+            shap_i=split_shapley(split_i,data)
+            all_shape.append(shap_i)
+        shap_arr=np.concatenate(all_shape,axis=0)
+        shap_matrix=np.mean(shap_arr,axis=0)
+        np.savetxt(out_path, shap_matrix, fmt='%f')
+    helper(in_path,out_path)
+
+def split_shapley(split,data):
+    train,test=split(data)
+    clf=RandomForestClassifier(class_weight="balanced")
+    clf.fit(train.X,train.y)
+    explainer = shap.Explainer(clf)
+    shap_values = explainer(test.X)
+    return shap_values.values
 
 def compute_shapley(in_path,out_path=None):
     if(out_path):
@@ -41,30 +69,13 @@ def xboost_basic(in_path,out_path=None):
 #        shap.plots.waterfall(shap_values[0])
         shap.plots.beeswarm(shap_values)
 
-def xboost_shapley(in_path,out_path=None):
-    if(out_path):
-        utils.make_dir(out_path)
-    for path_i in utils.top_files(in_path):
-        data_i=dataset.read_csv(path_i)
-        
-        model = xgboost.XGBRegressor().fit(data_i.X,data_i.y)
-        explainer = shap.Explainer(model)
-        shap_values = explainer(data_i.X)
-        print(path_i)
-
-#def show_prop(in_path):
-#    def helper(id,path):
-#        values = np.loadtxt(path)
-#        np.arg
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--compute', action='store_true')
-    parser.add_argument("--heat", type=str,default="shapley")
+    parser.add_argument("--input", type=str,default="uci/ineq")
+    parser.add_argument("--split", type=str,default="uci/output")
+    parser.add_argument("--output", type=str,default="test/shapley")
     args=parser.parse_args()
-    if(args.compute):
-#        raise Exception("Wrong")
-        xboost_shapley([#"AutoML/data",
-                        "uci/data"],"shapley/xboost")	
+    rf_shapley( in_path=args.input,
+                split_path=args.split,
+                out_path=args.output)
 #    show_heatmap(args.heat)
