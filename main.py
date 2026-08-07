e4a3747cdf2ee9b4c328bab1d8174a5f963f6adc
tjacek <jacekt89@gmail.com>
2026-08-07 21:47:52 +0200

    new ablation

--- main.py
+++ main.py
@@ -14,8 +14,23 @@ def make_splits( in_path,
                                        n_splits=n_splits)
         utils.make_dir(out_i)
         splits_i.save(f"{out_i}/splits")
-#        print(in_i)
+
+def ablation( in_path,
+	          out_path,
+	          clf_type="RF"):
+    clf_type=clfs.TYPES[clf_type]
+    for in_i,out_i in utils.out_iter(in_path,
+    	                             out_path):
+        data_i=dataset.read_csv(in_i)
+        splits_i=base.SplitGroup.read(f"{out_i}/splits")
+        ablat_i=f"{out_i}/ablat"
+        utils.make_dir(ablat_i)
+        print(in_i)
+        for j in range(data_i.dim()):
+            data_j=data_i.remove_col(j)
+            result_j,_=splits_i(data_j,clf_type)
+            result_j.save(f"{ablat_i}/{j}")
 
 #print(clfs.TYPES)
-make_splits("selected/data",
+ablation("selected/data",
 	        "selected/output")
