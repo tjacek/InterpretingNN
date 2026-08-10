1ac0419b2f978bec9bfcc396be731bad8e828142
tjacek <jacekt89@gmail.com>
2026-08-10 23:18:17 +0200

    lazy shapley

--- shape.py
+++ shape.py
@@ -1,6 +1,6 @@
 import numpy as np
 import shap
-import argparse
+import argparse,os.path
 import clfs
 import dataset
 import main
@@ -14,10 +14,15 @@ def compute_shapley(  data_path,
     data=dataset.read_csv(data_path)
     results,models=splits(data,clf_type)
     shapley_path=dir_proxy.dispatch("shapley")
-#    all_shape=[]
     for i,(split_i,model_i) in enumerate(zip(splits.splits,models)):
+        out_i=f"{shapley_path}/{i}"
+        print(out_i)
+        if os.path.exists(out_i+".npz"):
+            continue
         values_i=shape_split(data,split_i,model_i)
-        np.savez(f"{shapley_path}/{i}", values_i)
+        np.savez(out_i, values_i)
+
+
 #        all_shape.append(values_i)
 #    shap_arr=np.concatenate(all_shape,axis=0)
 #    shap_matrix=np.mean(shap_arr,axis=0)
