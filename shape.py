349476c4cf520de0e6acdbadbba6768e18ad107b
tjacek <tjacek@agh.edu.pl>
2026-08-16 21:17:36 +0200

    shapley exp

--- shape.py
+++ shape.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 import argparse,os.path
 import clfs
 import dataset
-import main,plot,utils
+import base,main,plot,utils
 
 @dataclass
 class ShapleyExp:
@@ -22,53 +22,35 @@ class ShapleyExp:
     def get_clf(self):
         return clfs.TYPES[self.clf_type]
 
+    def iter_exp(self,atrr,values):
+        for value_i in values:
+            exp_i=self.copy()
+            value_i=setattr(self, attr, value_i)
+            yield exp_i
+
 def compute_shapley(shap_exp):
     data,splits=shap_exp.get_data()
     clf_type=shap_exp.get_clf()
+    def helper(split_i,clf_i):
+        train,test=data.divide(split_i)
+        if(shap_exp.k is None):
+            background_data=train.X
+        else:
+            kmeans_summary = shap.kmeans( train.X, 
+                                          shap_exp.k)
+            background_data = kmeans_summary.data     
+        explainer=shap.Explainer( clf_i.model.predict_proba, 
+                        train.X)
+        shap_values = explainer(test.X)#,max_evals=620)
+        return shap_values.values
+    utils.make_dir(shap_exp.out_path)
     for i,split_i in enumerate(splits):
-        out_i=f"{shapley_path}/{i}"
+        out_i=f"{exp.out_path}/{i}"
         if os.path.exists(out_i+".npz"):
             continue
         clf_i,_=split_i.fit_clf(data,clf_type())
-        values_i=shape_split( data,
-                              split_i,
-                              clf_i
-                              k=shap_exp.k)
+        values_i=helper(split_i,clf_i)
         np.savez(out_i, values_i)
-
-def _compute_shapley(  data_path,
-	                  dir_path,
-	                  clf="TabPNF"):
-    dir_proxy=main.DirProxy(dir_path,clf)
-    clf_type=clfs.TYPES[clf]
-    data=dataset.read_csv(data_path)
-    shapley_path=dir_proxy.dispatch("shapley")
-    splits=dir_proxy.get_splits()
-    for i,split_i in enumerate(splits):
-        out_i=f"{shapley_path}/{i}"
-        print(out_i)
-        if os.path.exists(out_i+".npz"):
-            continue
-        clf_i,_=split_i.fit_clf(data,clf_type())
-        values_i=shape_split(data,split_i,clf_i)
-        np.savez(out_i, values_i)
-
-def shape_split( data_i,
-                 split_i,
-                 clf_i,
-                 k=100):
-    train,test=data_i.divide(split_i)
-    print(model_i.NAME)
-#    explainer=model_i.get_explainer(train.X)
-    if(k is None):
-        background_data=train.X
-    else:
-        kmeans_summary = shap.kmeans(train.X, k)
-        background_data = kmeans_summary.data     
-    shap.Explainer( clf_i.model.predict_proba, 
-                    train.X)
-    shap_values = explainer(test.X)#,max_evals=620)
-    return shap_values.values
 
 def show_shapley(dir_path):
     for dir_i in main.DirProxy.all_clfs(dir_path):
@@ -90,12 +72,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", type=str,default="selected/data/cleveland")
     parser.add_argument("--dir_path", type=str,default="selected/output/cleveland")
-    parser.add_argument("--clf", type=str,default="TabPNF")
+    parser.add_argument("--clf", type=str,default="RF")
     parser.add_argument("--cmd", type=str,default="compute")
     args=parser.parse_args()
     if(args.cmd=="compute"):
-         compute_shapley( args.data_path,
-	                      args.dir_path,
-	                      args.clf)
+        utils.make_dir(f"{args.dir_path}/{args.clf}")
+        exp=ShapleyExp( args.data_path,      
+                        f"{args.dir_path}/splits",
+                        args.clf,
+                        f"{args.dir_path}/{args.clf}/shapley",
+                        k=None)
+        compute_shapley( exp)
     if(args.cmd=="show"):
         show_shapley(  args.dir_path)
